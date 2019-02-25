6301a548f78b9afc97ef3984fc0a2096cd0d74c1
Kat <kat.m.borowska@gmail.com>
2019-02-25 14:33:55 +0100

    add route '/login'

--- app.py
+++ app.py
@@ -2,10 +2,30 @@ from flask import Flask, url_for, flash, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('mainpage.html')
+
+
+@app.route('/login', methods=[ 'POST'])
+def show_mainpage():
+    username= request.form.get['username']
+    avatar = request.form.get['avatar']
+
+    user= {
+        'username': username,
+        'avatar':avatar
+          }
+
+    return render_template('gameplay.html',
+                           user=user)
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
