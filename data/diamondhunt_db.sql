a0d7396821c5500eccdb78b3e37760a51231256c
Kat <kat.m.borowska@gmail.com>
2019-02-25 14:02:54 +0100

    add data (sql file)

---
+++ data/diamondhunt_db.sql
@@ -0,0 +1,24 @@
+-- Database: diamondhunt_db
+
+-- DROP DATABASE diamondhunt_db;
+
+CREATE DATABASE diamondhunt_db
+  WITH
+  OWNER = katarzynaborowska
+  ENCODING = 'UTF8'
+  LC_COLLATE = 'en_US.UTF-8'
+  LC_CTYPE = 'en_US.UTF-8'
+  TABLESPACE = pg_default
+  CONNECTION LIMIT = -1;
+
+
+DROP TABLE IF EXISTS user_score;
+
+CREATE TABLE user_score (
+  id serial PRIMARY KEY,
+  username varchar(250) NOT NULL,
+  score int,
+  date_time timestamp without time zone
+)
+
+
