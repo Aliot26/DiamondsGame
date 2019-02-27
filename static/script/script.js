61baad908a1bdfa0ef6e0b41dc5c1a70448e3aee
Kat <kat.m.borowska@gmail.com>
2019-02-27 11:56:11 +0100

    add user to dabase working

---
+++ static/script/script.js
@@ -0,0 +1,23 @@
+let id;
+
+function allowDrop(ev) {
+    ev.preventDefault();
+
+}
+
+function dragStart(ev) {
+
+    id = ev.target.id;
+
+
+}
+
+function drop(ev){
+    ev.target.append(document.getElementById(id))
+}
+
+function dropEnd(ev) {
+    ev.target.append('YAY')
+}
+
+
