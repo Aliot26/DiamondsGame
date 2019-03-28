62bb2a1b6050154c162678b0840050293465c963
Michal Kalinowski <subaru.kali@gmail.com>
2019-03-28 12:00:18 +0100

    pwa prepare try 14

--- static/addDiamond.js
+++ static/addDiamond.js
@@ -1,29 +1,29 @@
+// //
+// // function checkIfOnline() {
+// // 	console.log('beforeIf');
+// // 	if (!navigator.onLine) {
+// // 		clearContent();
+// // 		addDiamond();
+// // 	} else {
+// // 		console.log('online');
+// // 	}
+// // }
 //
-// function checkIfOnline() {
-// 	console.log('beforeIf');
-// 	if (!navigator.onLine) {
-// 		clearContent();
-// 		addDiamond();
-// 	} else {
-// 		console.log('online');
-// 	}
+// function clearContent() {
+// 	let content = document.getElementById('offline-content');
+// 	content.innerHTML = '';
 // }
-
-function clearContent() {
-	let content = document.getElementById('offline-content');
-	content.innerHTML = '';
-}
-
-function addDiamond() {
-	let content = document.getElementById('offline-content');
-	let diamondImage = content.createElement('img');
-	let diamondPath = 'static/image/diamond_pwa.png';
-
-	diamondImage.classList.add('diamondImage');
-	diamondImage.setAttribute('src', diamondPath);
-	diamondImage.setAttribute('alt', 'diamond image');
-
-	content.appendChild(diamondImage);
-}
-
-// checkIfOnline();
+//
+// function addDiamond() {
+// 	let content = document.getElementById('offline-content');
+// 	let diamondImage = content.createElement('img');
+// 	let diamondPath = 'static/image/diamond_pwa.png';
+//
+// 	diamondImage.classList.add('diamondImage');
+// 	diamondImage.setAttribute('src', diamondPath);
+// 	diamondImage.setAttribute('alt', 'diamond image');
+//
+// 	content.appendChild(diamondImage);
+// }
+//
+// // checkIfOnline();
