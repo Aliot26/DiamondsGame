20cd2433759e9a2188efd51d39e744d49f5e399e
Michal Kalinowski <subaru.kali@gmail.com>
2019-03-28 13:37:55 +0100

    pwa prepare try 23

--- static/script/gamescript.js
+++ static/script/gamescript.js
@@ -227,9 +227,10 @@ function checkIfOnline() {
 		console.log('offline');
         let content = document.getElementById('offline-content');
         console.log(typeof content);
-        while (content.firstChild) {
-            content.remove(content.firstChild)
-        }
+        // while (content.firstChild) {
+        //     content.remove(content.firstChild)
+        // }
+
 
         let diamondImage = content.createElement('img');
         let diamondPath = 'static/image/diamond_pwa.png';
